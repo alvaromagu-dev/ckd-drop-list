e0f523a222a712d57c7a7ffa9b6edc7acef279d2
Alvaro <alvaromagu2002@gmail.com>
2023-04-24 22:21:24 +0200

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
+import {FormArray, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reorderable-list';
+  readonly list = [
+    { name: 'Alvaro Martinez Gutierrez' },
+    { name: 'Fernando Alonso Díaz' },
+    { name: 'Lionel Andrés Messi Cuchetini' },
+    { name: 'Cristiano Ronaldo Aveiro Dos Santos' },
+    { name: 'Mónica Gutierrez Alonso' },
+  ];
+
+  form: FormGroup<{detail: FormArray<FormControl<string | null>>}> = new FormGroup({
+    detail: new FormArray(this.list.map(it => new FormControl(it.name))),
+  });
+
+  drop($event: CdkDragDrop<any>) {
+    moveItemInArray(this.form.controls.detail.controls, $event.previousIndex, $event.currentIndex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {CdkDrag, CdkDragHandle, CdkDragPlaceholder, CdkDragPreview, CdkDropList} from "@angular/cdk/drag-drop";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    CdkDropList,
+    CdkDrag,
+    CdkDragPlaceholder,
+    CdkDragPreview,
+    ReactiveFormsModule,
+    CdkDragHandle
   ],
   providers: [],
   bootstrap: [AppComponent]
